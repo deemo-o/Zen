b206c293fd1c1b085d87c78f46b9fceb70869690
deemo-o <deemohuang@gmail.com>
2023-01-02 18:51:46 -0500

    db, operations, few commands

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,55 +1,217 @@
+import random
 import discord
 from discord.ext import commands
 from discord import app_commands
-from database_utils import economy_database
+from economy_utils import dboperations
 
 class Economy(commands.Cog, description="Economy commands."):
-    
+
     def __init__(self, client: commands.Bot):
         self.client = client
-        try:
-            self.connection = economy_database.connect()
-            print("Connected to database")
-        except:
-            print("Couldn't connect to database")
-        
+        self.starting_money = 500
+        self.displayed_currency = "$"
+        self.connection = dboperations.connection()
+
+    def economy_embed(self, ctx: commands.Context) -> discord.Embed:
+        embed = discord.Embed(title="Zen | Economy", color=ctx.author.color)
+        return embed
+
+    def db_exception_embed(self, ctx: commands.Context, operation):
+        if isinstance(operation, Exception):
+            embed = self.economy_embed(ctx)
+            embed.description = "You already have an account!" if str(operation) == "UNIQUE constraint failed: members.userid" else str(operation).capitalize()
+            return embed
+            
+    async def cog_command_error(self, ctx: commands.Context, error: str):
+        embed = self.economy_embed(ctx)
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed.description = str(error).capitalize()
+            return await ctx.send(embed=embed)
+        if isinstance(error, commands.MemberNotFound):
+            embed.description = str(error).capitalize()
+            return await ctx.send(embed=embed)
+        if isinstance(error, commands.BadArgument):
+            embed.description = str(error).capitalize()
+            return await ctx.send(embed=embed)
+
     @commands.Cog.listener()
     async def on_ready(self):
         print("Economy module has been loaded.")
-        try:
-            economy_database.create_table(self.connection)
-            print("Table created")
-        except:
-            print("Something went wrong!")
+        dboperations.create_tables(self.connection)
+
+    @commands.command(aliases=["bal", "money", "networth"])
+    async def balance(self, ctx: commands.Context, member: discord.Member = None):
+        embed = self.economy_embed(ctx)
+        if member is None:
+            if dboperations.check_member_exists(self.connection, ctx.author):
+                money = dboperations.get_member_money(self.connection, ctx.author)
+                embed.description = f"You currently have **{money}**{self.displayed_currency}"
+                return await ctx.send(embed=self.db_exception_embed(ctx, money)) if self.db_exception_embed(ctx, money) else await ctx.send(embed=embed)
+            embed.description = f"{ctx.author.mention} doesn't have an account!\nType {self.client.command_prefix}createaccount to create an account."
+            return await ctx.send(embed=embed)
+        else:
+            if dboperations.check_member_exists(self.connection, member):
+                money = dboperations.get_member_money(self.connection, member)
+                embed.description = f"{member.mention} has **{money}**{self.displayed_currency}"
+                return await ctx.send(embed=self.db_exception_embed(ctx, money)) if self.db_exception_embed(ctx, money) else await ctx.send(embed=embed)
+            embed.description = f"{member.mention} doesn't have an account!\nType {self.client.command_prefix}createaccount to create an account."
+            return await ctx.send(embed=embed)
 
     @commands.command()
-    async def balance(self, ctx: commands.Context):
-        try:
-            data = economy_database.get_member(self.connection, ctx.author.id)
-            await ctx.send(f"You currently have {data[0][3]}$")
-            print(data)
-        except Exception as e:
-            print(e)
+    async def addmoney(self, ctx: commands.Context, amount: int, member: discord.Member = None):
+        embed = self.economy_embed(ctx)
+        if amount < 0:
+            embed.description = "The amount has to be positive!"
+            return await ctx.send(embed=embed)
+        if member is None:
+            if dboperations.check_member_exists(self.connection, ctx.author):
+                money = dboperations.add_member_money(self.connection, ctx.author, amount)
+                embed.description = f"Added **{amount}**{self.displayed_currency} to {ctx.author.mention}'s account!"
+                return await ctx.send(embed=self.db_exception_embed(ctx, money)) if self.db_exception_embed(ctx, money) else await ctx.send(embed=embed)
+            embed.description = f"{ctx.author.mention} doesn't have an account!\nType {self.client.command_prefix}createaccount to create an account."
+            return await ctx.send(embed=embed)
+        else:
+            if dboperations.check_member_exists(self.connection, member):
+                money = dboperations.add_member_money(self.connection, member, amount)
+                embed.description = f"Added **{amount}**{self.displayed_currency} to {member.mention}'s account!"
+                return await ctx.send(embed=self.db_exception_embed(ctx, money)) if self.db_exception_embed(ctx, money) else await ctx.send(embed=embed)
+            embed.description = f"{member.mention} doesn't have an account!\nType {self.client.command_prefix}createaccount to create an account."
+            return await ctx.send(embed=embed)
 
     @commands.command()
-    async def addmoney(self, ctx: commands.Context, amount: int):
-        try:
-            economy_database.update_member(self.connection, ctx.author.id, amount)
-            await ctx.send(f"Added {amount} to {ctx.author.name}'s account!")
-        except Exception as e:
-            print(e)
+    async def removemoney(self, ctx: commands.Context, amount: int, member: discord.Member = None):
+        embed = self.economy_embed(ctx)
+        if amount < 0:
+            embed.description = "The amount has to be positive!"
+            return await ctx.send(embed=embed)
+        if member is None:
+            if dboperations.check_member_exists(self.connection, ctx.author):
+                networth = dboperations.get_member_money(self.connection, ctx.author)
+                if amount > networth:
+                    amount = networth
+                money = dboperations.add_member_money(self.connection, ctx.author, -amount)
+                embed.description = f"Removed **{amount}**{self.displayed_currency} from {ctx.author.mention}'s account!"
+                return await ctx.send(embed=self.db_exception_embed(ctx, money)) if self.db_exception_embed(ctx, money) else await ctx.send(embed=embed)
+            embed.description = f"{ctx.author.mention} doesn't have an account!\nType {self.client.command_prefix}createaccount to create an account."
+            return await ctx.send(embed=embed)
+        else:
+            if dboperations.check_member_exists(self.connection, member):
+                networth = dboperations.get_member_money(self.connection, ctx.author)
+                if amount > networth:
+                    amount = networth
+                money = dboperations.add_member_money(self.connection, member, -amount)
+                embed.description = f"Removed **{amount}**{self.displayed_currency} from {member.mention}'s account!"
+                return await ctx.send(embed=self.db_exception_embed(ctx, money)) if self.db_exception_embed(ctx, money) else await ctx.send(embed=embed)
+            embed.description = f"{member.mention} doesn't have an account!\nType {self.client.command_prefix}createaccount to create an account."
+            return await ctx.send(embed=embed)
+
+    @commands.command()
+    async def work(self, ctx: commands.Context):
+        embed = self.economy_embed(ctx)
+        if dboperations.check_member_exists(self.connection, ctx.author):
+            rank = dboperations.get_member_rank(self.connection, ctx.author)
+            minmax = dboperations.get_rank_minmax_salary(self.connection, rank)
+            salary = random.randint(minmax[0], minmax[1])
+            operation = dboperations.add_member_money(self.connection, ctx.author, salary)
+            embed.description = f"You worked hard today and earned **{salary}**{self.displayed_currency} as {rank.capitalize()}!"
+            return await ctx.send(embed=self.db_exception_embed(ctx, operation)) if self.db_exception_embed(ctx, operation) else await ctx.send(embed=embed)
+        embed.description = embed.description = f"{ctx.author.mention} doesn't have an account!\nType {self.client.command_prefix}createaccount to create an account."
+        return await ctx.send(embed=embed)
 
     @commands.command()
     async def createaccount(self, ctx: commands.Context):
-        try:
-            economy_database.add_member(self.connection, ctx.author.id, ctx.author.name, 500)
-            await ctx.send("Account created with 500$")
-        except Exception as e:
-            print(e)
-            if str(e) == "UNIQUE constraint failed: members.userid":
-                return await ctx.send("You already have an account!")
-            else:
-                return await ctx.send("There is an issue right now, sorry!")
+        embed = self.economy_embed(ctx)
+        rank = dboperations.get_rank_with_position(self.connection, 1)
+        if not rank:
+            embed.description = "A staff member needs to set up the default rank before members can create an account."
+            return await ctx.send(embed=embed)
+        operation = dboperations.create_member(self.connection, ctx.author.id, ctx.author.name, self.starting_money, rank[0][1])
+        embed.description = f"Account created with **{self.starting_money}**{self.displayed_currency}, starting as {rank[0][1].capitalize()}."
+        return await ctx.send(embed=self.db_exception_embed(ctx, operation)) if self.db_exception_embed(ctx, operation) else await ctx.send(embed=embed)
 
+    @commands.command()
+    async def deleteaccount(self, ctx: commands.Context):
+        embed = self.economy_embed(ctx)
+        operation = dboperations.delete_member(self.connection, ctx.author.id)
+        embed.description = "Your account has been succesfully deleted!"
+        return await ctx.send(embed=self.db_exception_embed(ctx, operation)) if self.db_exception_embed(ctx, operation) else await ctx.send(embed=embed)
+
+    @commands.command()
+    async def createrank(self, ctx: commands.Context, rank: str, minsalary: int, maxsalary: int, required: int = 0, position: int = 1):
+        embed = self.economy_embed(ctx)
+        operation = dboperations.create_rank(self.connection, rank.lower(), minsalary, maxsalary, required, position)
+        embed.description = f"Created {rank.capitalize()} with a minimum daily salary of {minsalary}{self.displayed_currency} and a maximum daily salary of {maxsalary}{self.displayed_currency}."
+        return await ctx.send(embed=self.db_exception_embed(ctx, operation)) if self.db_exception_embed(ctx, operation) else await ctx.send(embed=embed)
+
+    # @commands.command()
+    # async def modifyrank(self, ctx: commands.Context, position: int = 1):
+    #     embed = self.economy_embed(ctx)
+
+    @commands.command()
+    async def deleterank(self, ctx: commands.Context, rank):
+        embed = self.economy_embed(ctx)
+        default_rank = dboperations.get_default_rank(self.connection)[0][1]
+        if rank == default_rank:
+            embed.description = f"You cannot delete the default rank!\nUse {self.client.command_prefix}modifyrank to modify the default rank"
+            return await ctx.send(embed=embed)
+        operation = dboperations.delete_rank(self.connection, rank.lower())
+        embed.description = f"You have successfully deleted {rank.capitalize()}!"
+        return await ctx.send(embed=self.db_exception_embed(ctx, operation)) if self.db_exception_embed(ctx, operation) else await ctx.send(embed=embed)
+
+    @commands.command()
+    async def startingmoney(self, ctx: commands.Context, money: int = None):
+        embed = self.economy_embed(ctx)
+        if money is None:
+            embed.description = f"The starting money is currently **{self.starting_money}**{self.displayed_currency}"
+        else:
+            self.starting_money = money
+            embed.description = f"Successfully changed starting money to **{money}**{self.displayed_currency}"
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def currency(self, ctx: commands.Context, currency = None):
+        embed = self.economy_embed(ctx)
+        if currency is None:
+            embed.description = f"The current currency is {self.displayed_currency}"
+        else:
+            self.displayed_currency = currency
+            embed.description = f"Successfully changed the currency to {currency}!"
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def profile(self, ctx: commands.Context, member: discord.Member = None):
+        embed = self.economy_embed(ctx)
+        if member is None:
+            if dboperations.check_member_exists(self.connection, ctx.author):
+                member_data = dboperations.get_member(self.connection, ctx.author)[0]
+                embed.set_thumbnail(url=ctx.author.avatar)
+                embed.add_field(name="Account Name", value=f"**{member_data[2]}**", inline=False)
+                embed.add_field(name="Net Worth", value=f"**{member_data[3]}**{self.displayed_currency}", inline=False)
+                embed.add_field(name="Rank", value=f"**{member_data[4].capitalize()}**", inline=False)
+
+                return await ctx.send(embed=self.db_exception_embed(ctx, member_data)) if self.db_exception_embed(ctx, member_data) else await ctx.send(embed=embed)
+            embed.description = embed.description = f"{ctx.author.mention} doesn't have an account!\nType {self.client.command_prefix}createaccount to create an account."
+            return await ctx.send(embed=embed)
+        else:
+            if dboperations.check_member_exists(self.connection, member):
+                member_data = dboperations.get_member(self.connection, member)[0]
+                embed.add_field(name="Account Name", value=f"**{member_data[2]}**", inline=False)
+                embed.add_field(name="Net Worth", value=f"**{member_data[3]}**{self.displayed_currency}", inline=False)
+                embed.add_field(name="Rank", value=f"**{member_data[4].capitalize()}**", inline=False)
+
+                return await ctx.send(embed=self.db_exception_embed(ctx, member_data)) if self.db_exception_embed(ctx, member_data) else await ctx.send(embed=embed)
+            embed.description = embed.description = f"{member.mention} doesn't have an account!\nType {self.client.command_prefix}createaccount to create an account."
+            return await ctx.send(embed=embed)
+
+    @commands.command()
+    async def leaderboard(self, ctx: commands.Context):
+        embed = self.economy_embed(ctx)
+        embed.description = ""
+        members_data = dboperations.get_leaderboard(self.connection)
+        print(members_data)
+        for index, member in enumerate(members_data):
+            embed.description += f"{index + 1}. <@{member[1]}> - **{member[3]}**{self.displayed_currency}\n"
+        
+        await ctx.send(embed=embed)
 async def setup(client):
     await client.add_cog(Economy(client))
--- database_utils/economy_database.py
+++ database_utils/economy_database.py
@@ -1,31 +1,58 @@
 import sqlite3 
-
-CREATE_MEMBERS_TABLE = "CREATE TABLE IF NOT EXISTS members (id INTEGER PRIMARY KEY, userid INTEGER UNIQUE, name TEXT, money INTEGER);"
-INSERT_MEMBER = "INSERT INTO members (userid, name, money) VALUES (?, ?, ?);"
-UPDATE_MEMBER_MONEY = "UPDATE members SET money = ? WHERE userid = ?;"
-GET_ALL_MEMBERS = "SELECT * FROM members;"
-GET_MEMBER_BY_USERID = "SELECT * FROM members WHERE userid = ?;"
+from database_utils import queries
 
 def connect():
     return sqlite3.connect("database_utils/economy.db")
 
-def create_table(connection):
+def create_members_table(connection):
     with connection:
-        connection.execute(CREATE_MEMBERS_TABLE)
+        connection.execute(queries.CREATE_MEMBERS_TABLE)
 
-def add_member(connection, userid, name, money):
+def create_ranks_table(connection):
     with connection:
-        connection.execute(INSERT_MEMBER, (userid, name, money))
+        connection.execute(queries.CREATE_RANKS_TABLE)
 
-def update_member(connection, userid, money):
-    money += get_member(connection, userid)[0][3]
+def add_member(connection, userid, name, money, rank):
     with connection:
-        connection.execute(UPDATE_MEMBER_MONEY, (money, userid))
+        connection.execute(queries.INSERT_MEMBER, (userid, name, money, rank))
+
+def add_rank(connection, name, minsalary, maxsalary, required, position):
+    with connection:
+        connection.execute(queries.INSERT_RANK, (name, minsalary, maxsalary, required, position))
+
+def add_member_money(connection, userid, money):
+    money += get_member_by_userid(connection, userid)[0][3]
+    with connection:
+        connection.execute(queries.UPDATE_MEMBER_MONEY, (money, userid))
 
 def get_all_members(connection):
     with connection:
-        return connection.execute(GET_ALL_MEMBERS).fetchall()
+        return connection.execute(queries.GET_ALL_MEMBERS).fetchall()
 
-def get_member(connection, userid):
+def get_all_members_by_networth(connection):
     with connection:
-        return connection.execute(GET_MEMBER_BY_USERID, (userid,)).fetchall()
+        return connection.execute(queries.GET_ALL_MEMBERS_BY_NETWORTH).fetchall()
+
+def get_all_ranks(connection):
+    with connection:
+        return connection.execute(queries.GET_ALL_RANKS).fetchall()
+
+def get_member_by_userid(connection, userid):
+    with connection:
+        return connection.execute(queries.GET_MEMBER_BY_USERID, (userid,)).fetchall()
+
+def get_rank_by_name(connection, name):
+    with connection:
+        return connection.execute(queries.GET_RANK_BY_NAME, (name,)).fetchall()
+
+def get_rank_by_position(connection, position):
+    with connection:
+        return connection.execute(queries.GET_RANK_BY_POSITION, (position,)).fetchall()
+
+def delete_member_by_userid(connection, userid):
+    with connection:
+        connection.execute(queries.DELETE_MEMBER, (userid,))
+
+def delete_rank_by_name(connection, name):
+    with connection:
+        connection.execute(queries.DELETE_RANK, (name,))
